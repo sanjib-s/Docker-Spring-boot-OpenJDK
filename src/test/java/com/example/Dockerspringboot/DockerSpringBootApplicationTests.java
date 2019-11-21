a4657e0319cdf65dcaa340d6fd6a220eaaaad8d5
sanjib-s <rush2sanjib1@gmail.com>
2019-11-21 16:19:42 -0500

    first commit

---
+++ src/test/java/com/example/Dockerspringboot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
